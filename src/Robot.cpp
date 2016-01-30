eed5743c24dcdbc9cdeeacb5d8841c66e18659f9
fvla <frederick.v.l@gmail.com>
2016-01-29 20:52:40 -0500

    Added a bunch of SmartDashboard::PutString statements.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,15 +5,33 @@
  */
 class Robot: public SampleRobot
 {
-
 public:
-	Robot()
-        {
-        }
+  Robot()
+  {
+    SmartDashboard::init();
+  }
 
-	void OperatorControl()
-	{
-	}
+  void OperatorControl()
+  {
+    SmartDashboard::PutString("DB/String 0", " ");
+    SmartDashboard::PutString("DB/String 1", " ");
+    while (IsOperatorControl() && IsEnabled())
+      {
+	SmartDashboard::PutString("DB/String 0", "Test");
+	SmartDashboard::PutString("DB/String 1", "Test");
+      }
+  }
+
+  void Test()
+  {
+    SmartDashboard::PutString("DB/String 0", " ");
+    SmartDashboard::PutString("DB/String 1", " ");
+    while (IsTest() && IsEnabled())
+      {
+	SmartDashboard::PutString("DB/String 0", "Test");
+	SmartDashboard::PutString("DB/String 1", "Test");
+      }
+  }
 };
 
 START_ROBOT_CLASS(Robot)
