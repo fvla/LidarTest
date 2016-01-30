7398c24d3c08c44c23537999d6906449ab697fdf
IanCoolidge0 <ian.coolidge@asdnh.org>
2016-01-30 14:31:09 -0500

    i2c member variable and communication

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -1,14 +1,23 @@
 #include "WPILib.h"
 
+#define I2C_SLAVE_ADR 0x53
+
 /**
  * This is a demo program showing how to use Mecanum control with the RobotDrive class.
  */
 class Robot: public SampleRobot
 {
+   I2C * i2cChannel;
+
 public:
   Robot()
   {
     SmartDashboard::init();
+    i2cChannel = new I2C(I2C::kOnboard, I2C_SLAVE_ADR);
+  }
+
+  ~Robot() {
+     delete i2cChannel;
   }
 
   void Test()
