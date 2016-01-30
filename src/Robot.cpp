f9ae73d5588db577fdc5f9f3c19fab7cc5ebdc1b
fvla <frederick.v.l@gmail.com>
2016-01-30 14:23:43 -0500

    Cleaned up Robot.cpp.

--- src/Robot.cpp
+++ src/Robot.cpp
@@ -11,25 +11,11 @@ public:
     SmartDashboard::init();
   }
 
-  void OperatorControl()
-  {
-    SmartDashboard::PutString("DB/String 0", " ");
-    SmartDashboard::PutString("DB/String 1", " ");
-    while (IsOperatorControl() && IsEnabled())
-      {
-	SmartDashboard::PutString("DB/String 0", "Test");
-	SmartDashboard::PutString("DB/String 1", "Test");
-      }
-  }
-
   void Test()
   {
-    SmartDashboard::PutString("DB/String 0", " ");
-    SmartDashboard::PutString("DB/String 1", " ");
     while (IsTest() && IsEnabled())
       {
 	SmartDashboard::PutString("DB/String 0", "Test");
-	SmartDashboard::PutString("DB/String 1", "Test");
       }
   }
 };
