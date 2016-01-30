8d0ecbb5315c2f61ee796766e26f553386a755a7
fvla <frederick.v.l@gmail.com>
2016-01-29 20:06:52 -0500

    Initialized robot repository.

---
+++ src/Robot.cpp
@@ -0,0 +1,19 @@
+#include "WPILib.h"
+
+/**
+ * This is a demo program showing how to use Mecanum control with the RobotDrive class.
+ */
+class Robot: public SampleRobot
+{
+
+public:
+	Robot()
+        {
+        }
+
+	void OperatorControl()
+	{
+	}
+};
+
+START_ROBOT_CLASS(Robot)
